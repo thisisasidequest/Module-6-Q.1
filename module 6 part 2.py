5bdbf9633e5ef7ab75bfac0cd0e1a55c4b10e397
thisisasidequest <104397733+thisisasidequest@users.noreply.github.com>
2022-06-26 23:39:26 -0400

    Add files via upload

---
+++ module 6 part 2.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+datetime_object = datetime.today()
+x = 60
+y = 2
+mydate = datetime.strftime(datetime_object, '%m/%d/%Y')
+currenttime = datetime.now().strftime(" %H:%M:%S")
+print (mydate)-(years=y)
+print (currenttime)-(minute=1)
